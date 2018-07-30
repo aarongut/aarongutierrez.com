deb7b875c77a98f7bbaf633c45b4bf7bcc945a23
Aaron Gutierrez <gutierrez.aaron.m@gmail.com>
2018-07-29 17:56:53 -0700

    fix some styles, bench 64

--- bench.sh
+++ bench.sh
@@ -6,8 +6,12 @@ set +x
 for FILE in img/bench/webp-src/*.jpg; do
 	OUT="${FILE%.*}.webp"
 	OUT2X="${FILE%.*}@2x.webp"
-	cwebp -mt -resize 160 0 -q 40 $FILE -o $OUT
-	cwebp -mt -q 40 $FILE -o $OUT2X
+	if [ ! -f ../$OUT ]; then
+		cwebp -mt -resize 160 0 -q 40 $FILE -o $OUT
+	fi
+	if [ ! -f ../$OUT2X ]; then
+		cwebp -mt -q 40 $FILE -o $OUT2X
+	fi
 done
 
 mv img/bench/webp-src/*.webp img/bench/
